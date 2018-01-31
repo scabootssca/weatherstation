d250fed033c7f72e26af132861e3cfb3c84f2c57
scabootssca <scabootssca@gmail.com>
2018-01-30 19:48:41 -0600

    Changed esp IP

--- Src/shared/config.h
+++ Src/shared/config.h
@@ -3,7 +3,7 @@
 
 #define SERVER_IP_ADDRESS "192.168.1.160"
 #define SERVER_PORT 80
-#define MY_IP 192, 168, 1, 180
+#define MY_IP 192, 168, 1, 190
 #define GATEWAY_IP 192, 168, 1, 1
 #define SUBNET_MASK 255, 255, 255, 0
 #define WIFI_SSID "MyCharterWiFi03-2G"
