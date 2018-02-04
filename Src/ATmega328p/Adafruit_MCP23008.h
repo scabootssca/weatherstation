8f1a7a55dd52f0097b005ba23e839990b1b91295
scabootssca <scabootssca@gmail.com>
2018-02-04 08:47:42 -0600

    Added mcp to atmega

---
+++ Src/ATmega328p/Adafruit_MCP23008.h
@@ -0,0 +1,69 @@
+/***************************************************
+  This is a library for the MCP23008 i2c port expander
+
+  These displays use I2C to communicate, 2 pins are required to
+  interface
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#ifndef _ADAFRUIT_MCP23008_H
+#define _ADAFRUIT_MCP23008_H
+
+#ifndef ONHIGH
+#define ONHIGH 0x05
+#endif
+
+// Don't forget the Wire library
+class Adafruit_MCP23008 {
+public:
+  void begin(uint8_t addr);
+  void begin(void);
+
+  void pinMode(uint8_t p, uint8_t d);
+  void digitalWrite(uint8_t p, uint8_t d);
+  void pullUp(uint8_t p, uint8_t d);
+  uint8_t digitalRead(uint8_t p);
+  uint8_t readGPIO(void);
+  void writeGPIO(uint8_t);
+
+  // Interrupt functions
+  void setInterruptOutPinMode(uint8_t mode);
+  bool interruptOn(uint8_t pin);
+  uint8_t readInterrupts();
+  void enableInterrupt(uint8_t pin, int mode=CHANGE);
+  void disableInterrupt(uint8_t pin);
+
+ private:
+  uint8_t i2caddr;
+  uint8_t read8(uint8_t addr);
+  void write8(uint8_t addr, uint8_t data);
+
+  uint8_t interruptHandlers[8];
+};
+
+#define MCP23008_ADDRESS 0x20
+
+// Interrupt pin modes
+#define MCP23008_INT_OUT_DRAIN 0x00
+#define MCP23008_INT_OUT_HIGH 0x01
+#define MCP23008_INT_OUT_LOW 0x02
+
+// registers
+#define MCP23008_IODIR 0x00
+#define MCP23008_IPOL 0x01
+#define MCP23008_GPINTEN 0x02
+#define MCP23008_DEFVAL 0x03
+#define MCP23008_INTCON 0x04
+#define MCP23008_IOCON 0x05
+#define MCP23008_GPPU 0x06
+#define MCP23008_INTF 0x07
+#define MCP23008_INTCAP 0x08
+#define MCP23008_GPIO 0x09
+#define MCP23008_OLAT 0x0A
+
+#endif
